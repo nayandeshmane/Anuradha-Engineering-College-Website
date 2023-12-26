9e85b36616003614be08a44c067b4f60e1bc6fb1
nayandeshmane <deshmanenayan05@gmail.com>
2023-12-26 21:34:00 +0530

    first commit

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+    $fullName = $_POST['fullName'];
+    $email = $_POST['email'];
+    $number = $_POST['number']; 
+    $birthdate = $_POST['birthdate'];
+    $gender = $_POST['gender'];
+    $address = $_POST['address'];
+    $address1 = $_POST['address1'];
+    $city= $_POST['city'];
+    $region = $_POST['region'];
+    $postelcode= $_POST['postelcode'];
+    $percent= $_POST['percent'];
+    $percent1 = $_POST['percent1'];
+     
+     //database connection
+?>
